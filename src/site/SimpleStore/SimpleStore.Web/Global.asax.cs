140898ab77226ba9e024215e373210b133df1a7b
pdb <fudder@hotmail.com>
2010-06-22 00:16:00 -0700

    Finished basic web build

--- src/site/SimpleStore/SimpleStore.Web/Global.asax.cs
+++ src/site/SimpleStore/SimpleStore.Web/Global.asax.cs
@@ -5,6 +5,23 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
+using Microsoft.Practices.ServiceLocation;
+using Castle.Windsor;
+using Castle.MicroKernel.Registration;
+using MvcContrib.Castle;
+using CommonServiceLocator.WindsorAdapter;
+using SharpArch.Data.NHibernate;
+using SharpArch.Web.NHibernate;
+using SharpArch.Web.Castle;
+using SharpArch.Web.ModelBinder;
+using SharpArch.Core.PersistenceSupport.NHibernate;
+using SharpArch.Core.PersistenceSupport;
+using SimpleStore.Data.NHibernateMaps;
+
+using SharpArch.Core.CommonValidator;
+using SharpArch.Core.NHibernateValidator.CommonValidatorAdapter;
+using SharpArch.Core.NHibernateValidator.ValidatorProvider;
+
 namespace SimpleStore.Web
 {
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
@@ -12,9 +29,12 @@ namespace SimpleStore.Web
 
     public class MvcApplication : System.Web.HttpApplication
     {
+        private WebSessionStorage webSessionStorage;
+
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{*favicon}", new { favicon = @"(.*/)?favicon.ico(/.*)?" });
 
             routes.MapRoute(
                 "Default", // Route name
@@ -24,11 +44,77 @@ namespace SimpleStore.Web
 
         }
 
+        public override void Init()
+        {
+            base.Init();
+
+            // The WebSessionStorage must be created during the Init() to tie in HttpApplication events
+            webSessionStorage = new WebSessionStorage(this);
+        }
+
         protected void Application_Start()
         {
+            log4net.Config.XmlConfigurator.Configure();
             AreaRegistration.RegisterAllAreas();
-
             RegisterRoutes(RouteTable.Routes);
+
+            DataAnnotationsModelValidatorProvider.AddImplicitRequiredAttributeForValueTypes = false;
+            ModelValidatorProviders.Providers.Add(new NHibernateValidatorProvider()); //Server side validation provider
+
+            ModelBinders.Binders.DefaultBinder = new SharpModelBinder();
+
+            IWindsorContainer container = new WindsorContainer();
+            ControllerBuilder.Current.SetControllerFactory(new WindsorControllerFactory(container));
+            container.RegisterControllers(typeof(SimpleStore.Web.Controllers.HomeController).Assembly);
+            //Generic Repositories
+            container.AddComponent("entityDuplicateChecker",
+                typeof(IEntityDuplicateChecker), typeof(EntityDuplicateChecker));
+            container.AddComponent("repositoryType",
+                typeof(IRepository<>), typeof(Repository<>));
+            container.AddComponent("nhibernateRepositoryType",
+                typeof(INHibernateRepository<>), typeof(NHibernateRepository<>));
+            container.AddComponent("repositoryWithTypedId",
+                typeof(IRepositoryWithTypedId<,>), typeof(RepositoryWithTypedId<,>));
+            container.AddComponent("nhibernateRepositoryWithTypedId",
+                typeof(INHibernateRepositoryWithTypedId<,>), typeof(NHibernateRepositoryWithTypedId<,>));
+
+            //Custom Repositories
+            container.Register(
+                AllTypes.Pick()
+                .FromAssemblyNamed("SimpleStore.Data")
+                .WithService.FirstNonGenericCoreInterface("SimpleStore.Core"));
+
+            //Validator
+            container.AddComponent("validator",
+                typeof(IValidator), typeof(Validator));
+
+            ServiceLocator.SetLocatorProvider(() => new WindsorServiceLocator(container));
+        }
+
+        /// <summary>
+        /// Due to issues on IIS7, the NHibernate initialization cannot reside in Init() but
+        /// must only be called once.  Consequently, we invoke a thread-safe singleton class to 
+        /// ensure it's only initialized once.
+        /// </summary>
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            NHibernateInitializer.Instance().InitializeNHibernateOnce(
+                () => InitializeNHibernateSession());
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            // Useful for debugging
+            Exception ex = Server.GetLastError();
+        }
+
+        private void InitializeNHibernateSession()
+        {
+            NHibernateSession.Init(
+                webSessionStorage,
+                new string[] { Server.MapPath("~/bin/SimpleStore.Data.dll") },
+                new AutoPersistenceModelGenerator().Generate(),
+                Server.MapPath("~/NHibernate.config"));
         }
     }
 }
